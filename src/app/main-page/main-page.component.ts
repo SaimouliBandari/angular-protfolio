53559136a7979842c47ee9879e079d5d36bcb94f
SaimouliBandari <saimouli4k@gmail.com>
2022-12-22 22:41:30 +0530

    removed deprications

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log(screen.width)
+   }
 
   ngOnInit(): void {
   }
+
+
 
 }
